b2f6c769cd901e8d451b01519129843877e8a9d1
Kate Summerill <kaitlynsummerill@gmail.com>
2019-11-18 16:51:12 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push(array, element)
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array.index(array, index_number)
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
